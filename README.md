38ef2fc96367203c90c1b13d17f8720bfd06dc8f
LaGregance <gregoire.taja@gmail.com>
2025-02-26 09:35:30 +0100

    Allow external link

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ This extension add an additional feature to Firefox Multi-Account Containers: wh
 it will reuse the container of the previous active tab.
 
 It help to keep environment separated will maintaining navigation easy, mainly for separate work & personal stuff.
+
+## Development
+
+Go to `about:debugging` -> This Firefox -> Load Temporary Add-on (select `manifest.json`).  
+To see log: Tools -> Browser Tools -> Browser Console (CMD+MAJ+J) -> Select Multiprocess
+
+## Release (Beta - GitHub)
+
+1. Update version in `manifest.json`
+2. Execute `web-ext build`
+3. Go to firefox addon developer Hub (https://addons.mozilla.org/en-US/developers/addon/002debba3c3946a98008/edit)
+4. Click "Upload New Version" -> Select the zip you just created with `web-ext build`
+5. Wait the signature to be done and download the xpi file (see tips bellow)
+6. Update `update_manifest.json` with your new build
+
+Then you can install the extension using raw github content (like https://raw.githubusercontent.com/LaGregance/firefox-container-reuse/refs/heads/main/web-ext-artifacts/container_reuse_prerelease-1.2.xpi)
+
+Tips: to download the xpi instead of trying to install it:
+- Go to `about:config`, set `browser.altClickSave` to true
+- Go back to the page and use alt+click on the xpi link
--- background.js
+++ background.js
@@ -2,34 +2,59 @@ const NO_CONTAINER = "firefox-default";
 const DEFAULT_CONTAINER_NAME = "firefox-container-2";
 const ACTIVE_CONTAINER_KEY = "CONTAINER_REUSE_ACTIVE";
 
-// TODO: Default doesn't work as tabs onCreated doesn't work
-
 async function getActiveContainer() {
-    const result = await browser.storage.local.get({ [ACTIVE_CONTAINER_KEY]: NO_CONTAINER })
+    const result = await browser.storage.local.get({ [ACTIVE_CONTAINER_KEY]: DEFAULT_CONTAINER_NAME })
     return result[ACTIVE_CONTAINER_KEY];
 }
 
 async function setActiveContainer(value) {
-    const result = await browser.storage.local.set({ [ACTIVE_CONTAINER_KEY]: value ?? NO_CONTAINER })
+    await browser.storage.local.set({ [ACTIVE_CONTAINER_KEY]: value ?? DEFAULT_CONTAINER_NAME })
 }
 
 browser.tabs.onActivated.addListener(async (event) => {
-    let tab = await browser.tabs.get(event.tabId);
-    setActiveContainer(tab.cookieStoreId);
+    try {
+        let tab = await browser.tabs.get(event.tabId);
+        if (tab.cookieStoreId !== NO_CONTAINER) {
+            await setActiveContainer(tab.cookieStoreId);
+        }
+    } catch (e) {
+        // Tab already deleted because it was on wrong container
+    }
 });
 
-browser.tabs.onCreated.addListener(async (tab) => {
-    const activeContainer = await getActiveContainer();
+browser.tabs.onCreated.addListener((tab) => {
+    const updater = async (tabId, changeInfo) => {
+        if (tabId === tab.id && changeInfo.url) {
+            browser.tabs.onUpdated.removeListener(updater);
 
-    if (activeContainer != NO_CONTAINER && tab.cookieStoreId == NO_CONTAINER) {
-        // Re-create the tab in the good container
-        browser.tabs.create({
-            index: tab.index,
-            openerTabId: tab.openerTabId,
-            cookieStoreId: activeContainer
-        });
-        
-        // Close the original tab
-        browser.tabs.remove(tab.id);
+            if (changeInfo.url === 'about:newtab') {
+                // about:newtab cause "Illegal URL: about:newtab"
+                changeInfo.url = undefined;
+            } else if (!changeInfo.url.startsWith('http://') && !changeInfo.url.startsWith('https://') && changeInfo.url !== 'about:blank') {
+                // Only apply container if link is http(s)
+                return;
+            }
+
+            const activeContainer = await getActiveContainer();
+            if (activeContainer !== NO_CONTAINER && tab.cookieStoreId === NO_CONTAINER) {
+                // Re-create the tab in the good container
+                const newTab = await browser.tabs.create({
+                    index: tab.index,
+                    openerTabId: tab.openerTabId,
+                    cookieStoreId: activeContainer,
+                    url: changeInfo.url,
+                });
+
+                // Close the original tab
+                await browser.tabs.remove(tab.id);
+            }
+        }
+    };
+
+    if (tab.url === "about:newtab") {
+        // Newtab will never receive URL update, so trigger directly
+        updater(tab.id, { url: tab.url }).catch(console.error);
+    } else {
+        browser.tabs.onUpdated.addListener(updater);
     }
 });
